0076d9fe09f6b316ac3ae00ecf46d0bd30031f79
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-06-02 15:55:33 +0530

    Add files via upload

--- assests/js/script.js
+++ assests/js/script.js
@@ -97,20 +97,6 @@ document.onkeydown = function(e) {
   }
 }
 
-// Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  (function(){
-  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  s1.async=true;
-  s1.src='https://embed.tawk.to/60f70460649e0a0a5ccd22a7/1fb2ei71o';
-  s1.charset='UTF-8';
-  s1.setAttribute('crossorigin','*');
-  s0.parentNode.insertBefore(s1,s0);
-  })();
-// End of Tawk.to Live Chat
-
-
-
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
     origin: 'top',
